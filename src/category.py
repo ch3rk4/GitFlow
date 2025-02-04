b27aae4136f88b7c240bc66d34aecc92415f3ab5
ch3rk4 <pety.shurupov@gmail.com>
2025-02-04 22:24:56 +0300

    add new funcs for class Category: products and add_product

--- src/category.py
+++ src/category.py
@@ -14,7 +14,16 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Any] = None) -> None:
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.categories_count += 1
         Category.products_count += len(products) if products else 0
+
+    @property
+    def products(self):
+        return f"{self.name}, {self.__products} руб. Остаток: {Category.products_count}"
+
+    def add_product(self, new_product):
+        """Добавляет товар в категорию"""
+        self.__products.append(new_product)
+        Category.products_count += 1
