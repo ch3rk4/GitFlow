3a86dbfaf2993a9c878d8662a41f95615e6ff34a
ch3rk4 <pety.shurupov@gmail.com>
2025-03-05 20:58:13 +0300

    add new classes Smartphone and LawnGrass

---
+++ src/Smartphone.py
@@ -0,0 +1,19 @@
+from src.product import Product
+
+class Smartphone(Product):
+    efficiency: str
+    model: str
+    memory: int
+    color: str
+
+    def __init__(
+            self, name: str, description: str, price: float, quantity: int,
+            efficiency: str, model: str, memory: int, color: str
+                 ) -> None:
+
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
